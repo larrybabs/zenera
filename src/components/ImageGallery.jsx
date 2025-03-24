77cef7179bf1a3631d5a172fb9959288bc6b9847
Samad <seshp@ymail.com>
2025-03-24 10:01:11 +0100

    make gallery grid 2 on mobile

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -133,7 +133,7 @@ const ImageGallery = () => {
       </div> */}
 
       {/* Image Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
         {filteredImages.map((image, index) => (
           <div
             key={`${image.name}-${index}`}
